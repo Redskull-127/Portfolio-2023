58ee852a7f6f3d6a379d086ec966c6848d076182
Redskull-127 <meerraja17@gmail.com>
2023-04-26 11:47:16 +0530

    Added Meta Description, Spotify API and optimized 🥹

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import '@/styles/globals.css'
-import Head from 'next/head'
-import { useEffect } from 'react'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import NextNProgress from 'nextjs-progressbar';
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -13,10 +13,21 @@ export default function App({ Component, pageProps }) {
     <>
       <NextNProgress />
       <Head>
-          <title>Meer Tarbani&apos;s Portfolio</title>
-          <link rel="icon" href="/Images/bitmoji.png" />
-        </Head>
+        <title>Meer Tarbani&apos;s Portfolio</title>
+        <link rel="icon" href="/Images/bitmoji.png" />
+        <meta name="description" content="Meer Tarbani Portfolio Website" />
+        <meta
+          name="keywords"
+          content="Meer Tarbani, Meer, Tarbani, Portfolio, Website, meer tarbani, meer tarbani acid, meer tarbani as a fraction, meer tarbani ba, meer tarbani bali, meer tarbani bangla, meer tarbani bangla lyrics, meer tarbani battery, meer tarbani bank, meer tarbani blood pressure, meer tarbani chords, meer tarbani com, meer tarbani code, meer tarbani center, meer tarbani de, meer tarbani delhi, meer tarbani dei, meer tarbani dit, meer tarbani definition, meer tarbani download, meer tarbani disease, meer tarbani english translation, meer tarbani english lyrics, meer tarbani english, meer tarbani english pdf, meer tarbani english subtitles, meer tarbani englisch, meer tarbani example, meer tarbani film, meer tarbani facebook, meer tarbani full movie, meer tarbani fakaza, meer tarbani font, meer tarbani file, meer tarbani for sale,  eer tarbani gif, meer tarbani google translate,  meer tarbani google scholar, meer tarbani google, meer tarbani google drive, meer tarbani games, meer tarbani germany,"
+        />
+      </Head>
       <Component {...pageProps} />
+      <iframe
+        src="https://novatorem-mauve-eight.vercel.app/api/spotify"
+        className="absolute top-0 left-[32%] iframe"
+        height={150}
+        width={750}
+      ></iframe>
     </>
-  )
+  );
 }
--- src/utils/Chips.jsx
+++ src/utils/Chips.jsx
@@ -1,6 +1,6 @@
 export default function Chips({ skill, href, description }) {
     return (
-        <a href={href} target={"_blank"} title={description} className="hover:bg-slate-600 select-none text-lg transition-all duration-200 cursor-pointer min-w-28 h-20 px-5 flex justify-center items-center rounded-lg bg-[#111827]">
+        <a href={href} target={"_blank"} title={description} className="hover:bg-slate-600 select-none text-lg transition-all duration-200 cursor-pointer min-w-28 h-20 px-5 flex flex-col justify-center items-center rounded-lg bg-[#111827]">
             <p>{skill}</p>
         </a>
     )
