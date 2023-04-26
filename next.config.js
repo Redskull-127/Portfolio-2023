18c636d35066f486d614ca25212c5b0be41c6a27
Redskull-127 <meerraja17@gmail.com>
2023-04-26 12:19:21 +0530

    configured next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
-};
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = withPWA({
-    pwa: {
-      dest: "public",
-      // disable: process.env.NODE_ENV === 'development',
-      disable: false,
-      register: true,
-      skipWaiting: true,
-    },
-  });
-} else {
-  module.exports = nextConfig;
 }
 
-
+module.exports = withPWA(nextConfig);
