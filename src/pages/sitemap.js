e26dc7c625aad5c9b576dfb5017af2dba74cbe87
Redskull-127 <meerraja17@gmail.com>
2023-04-29 00:30:58 +0530

    Added sitemap and robots ✨

--- src/pages/sitemap.js
+++
@@ -1,8 +0,0 @@
-export default async function sitemap() {
-    const routes = ['', '/skills','/latest','/projects','/contact','/support'].map(
-        (route) => ({
-            url: `https://meertarbani.in${route}`,
-        })
-    );
-    return [...routes]
-}
