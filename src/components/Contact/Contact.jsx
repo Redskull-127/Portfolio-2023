20af7a3233856a74590fa038170389d68c045817
Redskull-127 <meerraja17@gmail.com>
2023-05-07 16:35:43 +0530

    Merged Contact and Support Page!

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,11 @@ export default function ContactContent() {
             <div data-aos="fade-left" className="my-10 w-full">
                 <Chips skill={"Send me an email!"} description={"redskull@duck.com"} href={"mailto:redskull@duck.com"}/>
             </div>
+            <h1 data-aos="fade-left" className="text-5xl">Support</h1>
+            <div data-aos="fade-left" className="my-10 flex flex-col gap-5 w-full">
+                <Chips skill={"Buy Me a Coffee!"} href={"https://www.buymeacoffee.com/meertarbani"}/>
+                <Chips skill={"Support me through Stripe!"} href={"https://github.com/sponsors/Redskull-127?o=esc"}/>
+            </div>
         </div>
     )
 }
