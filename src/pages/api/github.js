076183177bba5e9711dd92272ddc54aaee219453
Redskull-127 <meerraja17@gmail.com>
2023-05-15 15:36:27 +0530

    Added error safety

--- src/pages/api/github.js
+++ src/pages/api/github.js
@@ -1,7 +1,11 @@
 export default async function GithubAPI(req, res) {
-  const data = await fetch(
-    "https://api.github.com/users/redskull-127/repos?sort=created"
-  ).then((res) => res.json());
-  res.status(200).json({ data: data });
-  // res.status(200).json({ name: 'John Doe' })
+  try{
+    const data = await fetch(
+      "https://api.github.com/users/redskull-127/repos?sort=created"
+    ).then((res) => res.json());
+    res.status(200).json({ data: data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
 }
--- src/pages/api/twitter.js
+++ src/pages/api/twitter.js
@@ -1,7 +1,12 @@
 export default async function TwitterAPI(req, res) {
-    const data = await fetch(
-      process.env.NEXT_PUBLIC_TWITTER_API
-    ).then((res) => res.json());
-    res.status(200).json({ data: data });
+    try{
+      const data = await fetch(
+        process.env.NEXT_PUBLIC_TWITTER_API
+      ).then((res) => res.json());
+      res.status(200).json({ data: data });
+    } catch(err) {
+      console.log(err);
+      res.status(500).send({ error: err.message });
+    }
   }
   
