18cafb330f0f0826b0b6430e5819a1e5234f324d
Redskull-127 <meerraja17@gmail.com>
2023-06-23 18:59:48 +0530

    alignment

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -72,7 +72,7 @@ export default function Song() {
                 height={150}
                 width={750}
             ></iframe>
-            <div className="absolute top-14 left-[92%] cursor-pointer">
+            <div className="absolute top-14 left-[64%] cursor-pointer">
                 {songUrl && isPlaying ? (
                     <div onClick={() => setIsPlaying(false)}>
                         <PauseIcon />
@@ -83,7 +83,7 @@ export default function Song() {
                     </div>
                 )}
             </div>
-            <Link href="https://open.spotify.com/user/to6rms2g0fzerpkwox1k4v33w" target={"_blank"} className="absolute top-14 left-[98%] cursor-pointer">
+            <Link href="https://open.spotify.com/user/to6rms2g0fzerpkwox1k4v33w" target={"_blank"} className="absolute top-14 left-[67%] cursor-pointer">
                 <Spotify />
             </Link>
             {songUrl != "" && (
